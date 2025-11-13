95947f0a8a3ca32cf5f35c4cd3060d5a5f1e9aa8
Biryukov Igor <isbirukov@gmail.com>
2025-11-13 16:14:12 +0300

    chore: Add cmd for spark

---
+++ spark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+docker run -d \
+  -p 8888:8888 \
+  -v ./practice:/home/jovyan/practice \
+  -v ./final_tasks:/home/jovyan/final_tasks \
+  -v ./learning:/home/jovyan/learning \
+  --restart unless-stopped \
+  quay.io/jupyter/pyspark-notebook:2025-04-26 \
+  start-notebook.sh --NotebookApp.token=''
